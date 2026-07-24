ff9b413d626554c76a8dbb4fb82a53c36b923821
ViShwas570 <141233834+ViShwas570@users.noreply.github.com>
2026-07-24 10:48:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Find length of Loop/find-length-of-loop.py
@@ -0,0 +1,23 @@
+'''
+class Node:
+    def __init__(self, data): 
+        self.data = data
+        self.next = None
+'''
+
+class Solution:
+    def lengthOfLoop(self, head):
+        #code here
+        slow=head
+        fast=head
+        while fast is not None and fast.next is not None:
+            slow=slow.next
+            fast=fast.next.next
+            if slow==fast:
+                slow=slow.next
+                count=1
+                while slow!=fast:
+                    slow=slow.next
+                    count+=1
+                return count
+        return 0
