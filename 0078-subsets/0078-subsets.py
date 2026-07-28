364c76cf76f5189b251bd16aa19acaf1a656bb44
ViShwas570 <141233834+ViShwas570@users.noreply.github.com>
2026-07-28 22:38:12 +0530

    Time: 0 ms (100%), Space: 19.4 MB (27.19%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,12 +1,27 @@
 class Solution:
+    def solve(self,nums,idx,subset,result):
+        if idx>=len(nums):
+            result.append(subset.copy())
+            return
+
+        subset.append(nums[idx])
+        self.solve(nums,idx+1,subset,result)
+        subset.pop()
+        self.solve(nums,idx+1,subset,result)
+
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        n=len(nums)
-        total_subsets=1<<n
         result=[]
-        for num in range(0,total_subsets):
-            list=[]
-            for i in range(0,n):
-                if num & (1<<i)!=0:
-                    list.append(nums[i])
-            result.append(list)
-        return result        
+        self.solve(nums,0,[],result)
+        return result
+        # n=len(nums)
+        # total_subsets=1<<n
+        # result=[]
+        # for num in range(0,total_subsets):
+        #     list=[]
+        #     for i in range(0,n):
+        #         if num & (1<<i)!=0:
+        #             list.append(nums[i])
+        #     result.append(list)
+        # return result   
+
+             
